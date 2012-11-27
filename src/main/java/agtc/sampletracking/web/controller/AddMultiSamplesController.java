24620127d9c3d5852386b1503b96781b7aacf5b5
imnderoo <nderoo.wong@gmail.com>
2012-11-27 17:16:14 -0500

    Register Multiple Sample functionality
    Controller Added

--- src/main/java/agtc/sampletracking/web/controller/AddMultiSamplesController.java
+++ src/main/java/agtc/sampletracking/web/controller/AddMultiSamplesController.java
@@ -9,6 +9,9 @@ package agtc.sampletracking.web.controller;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.RequestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -18,17 +21,20 @@ import agtc.sampletracking.model.*;
 import java.util.*;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.*;
 
 import org.springframework.validation.Errors;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.WebUtils;
 
 import agtc.sampletracking.bus.manager.AGTCManager;
 import agtc.sampletracking.bus.manager.ContainerManager;
 import agtc.sampletracking.bus.manager.ProjectManager;
 import agtc.sampletracking.bus.manager.SampleManager;
-import agtc.sampletracking.bus.report.SatoLabelPrinter;
-
+import agtc.sampletracking.bus.report.SatoLabelPrinter
+;
+import org.apache.commons.collections.list.LazyList;
+import org.apache.commons.collections.FactoryUtils;
 /**
  * @author Hongjing
  *
@@ -36,114 +42,93 @@ import agtc.sampletracking.bus.report.SatoLabelPrinter;
  * Window - Preferences - Java - Code Style - Code Templates
  */
 public class AddMultiSamplesController extends BasicController {
-	private Log log = LogFactory.getLog(AddSingleSampleController.class);
+	private Log log = LogFactory.getLog(AddMultiSamplesController.class);
 	private SampleManager sampleManager;
 	private ContainerManager containerManager;
 	private AGTCManager agtcManager;
 	private ProjectManager projectManager;
 	
 	public AddMultiSamplesController(){
-		//initialize the sform from the formBackingObject
+		//initialize the form from the formBackingObject
 		 setBindOnNewForm(true);
-	
 	}
 	
 	protected Object formBackingObject(HttpServletRequest request) throws ServletException {
-		
-			Sample sample = new Sample();
+
+			MultiSamples multiSampleClass = new MultiSamples();
+
+			return multiSampleClass;
 			
-			Patient patient = new Patient();
-			sample.setPatient(patient);
-			
-			SampleType sampleType = new SampleType();
-			sample.setSampleType(sampleType);
-			
-			sample.setSampleId(new Integer(-1));
-			return sample;
-		
 	}
 
-	protected ModelAndView onSubmit(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response, java.lang.Object command,BindException errors) throws Exception {
-		Sample sample = (Sample) command;
-		List<Sample> samples = new ArrayList<Sample>();
+	protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object command,BindException errors) throws Exception {
+		
+		MultiSamples multiSamples = (MultiSamples) command;
+
+		System.out.println("Entering onSubmit");
+		
+		// List to store results.
 		List<Sample> printSampleList = new ArrayList<Sample>();
-//		List allSampleTypes = agtcManager.getSampleTypesWithVials();
-//		Iterator ist = allSampleTypes.iterator();
-//		int counter = 0;
-		
-		if(sample.getPatient().getProject()==null){
-			errors.reject("error.required",new String[]{"sample source"},"Project is required");
-			return showForm(request, response, errors);
-		}
-		if(sample.getSampleType()==null){
-			errors.reject("error.required",new String[]{"sample type"},"Sample Type is required");
-			System.out.println("Sample Type Error.");
-			return showForm(request, response, errors);
-		}
-		
-//		Integer initialLabelNo = sample.getSampleType().getInitialLabelNo();
+		List<Sample> sampleList = multiSamples.getMultiSamples();
 
-//		if(initialLabelNo!=null){
-//		int b = initialLabelNo.intValue();
-//		for(int i=0;i<b;i++){
-//			printSampleList.add(sample);
-//		}
+		for (int i=0; i<sampleList.size(); i++)
+		{
+			Sample sample = sampleList.get(i);
+			
+			if(sample.getSampleType()==null){
+				errors.reject("error.required",new String[]{"sample type"},"Sample Type is required");
+					System.out.println("Sample Type Error.");
+					return showForm(request, response, errors);
+				}
+				
+				if(sample.getPatient().getProject()==null){
+					errors.reject("error.required",new String[]{"sample source"},"Project is required");
+					return showForm(request, response, errors);
+				}
 		
-		System.out.println("Attempting Print List Add.");
-		printSampleList.add(sample);
-		
-		System.out.println("Attempting Sample Add.");
-		samples.add(sample);
-		
-//		while(ist.hasNext()){
-//			SampleType st = (SampleType)ist.next();
-//			String suffix = st.getSuffix();
-//			String submitted = RequestUtils.getStringParameter(request, suffix,"");
-//			if(!submitted .equals("")){
-//				Sample s1 = (Sample)sample.clone();
-//			
-//				s1.setSampleType(st);
-//				samples.add(s1);
-//				counter++;
-//				Integer initialLabelNo = st.getInitialLabelNo();
-//				if(initialLabelNo!=null){
-//					int b = initialLabelNo.intValue();
-//					for(int i=0;i<b;i++){
-//						printSampleList.add(s1);
-//					}
-//				}
-//			}
-//		}
-		
+				// Add number of labels to print queue
+//				Sample st = agtcManager.getSampleType(sample)
+				Integer initialLabelNo = sample.getSampleType().getInitialLabelNo();
+				
+				System.out.println(initialLabelNo);
+				
+				if(initialLabelNo!=null){
+					int b = initialLabelNo.intValue();
+					for(int j=0;j<b;j++){
+						printSampleList.add(sample);
+					}
+				}
+			}
+			
+			String message = "";
+			
+			// Save samples to DB
+			try{
+				sampleManager.saveSamples(sampleList);
+				message += "Samples successfully added to database.";
+			}catch(Exception e){
+				log.debug(e.getMessage());
+					errors.rejectValue( "patient.intSampleId","error.notUnique",new String[]{"Internal Sample ID not unique for selected sample type "},"Not unique");
+				return showForm(request, response, errors);
+			}
 	
-		try{
-			System.out.println("Attempting Save.");
-			sampleManager.saveSamples(samples);
-		}catch(Exception e){
-			log.debug(e.getMessage());
-				errors.rejectValue( "patient.intSampleId","error.notUnique",new String[]{sample.getPatient().getIntSampleId()+ " for selected sample type "},"Not unique");
-			return showForm(request, response, errors);
-		}
-		String message = "Have successfully saved this sample !";
-		
-		//Print sample's label
-		//get label quantity
-		String labelnu = request.getParameter("labelNo");
-		if(labelnu!=null){
-			SatoLabelPrinter satoP = new SatoLabelPrinter();
-			satoP.printSatoLabel(printSampleList);
-			message += "<br> Please pick up your labels at the Label printer !";
-		}
-		
-		String intSampleId = sample.getPatient().getIntSampleId();
-		ModelAndView view = new ModelAndView(new RedirectView(getSuccessView()));
-		Map myModel = view.getModel();
-		myModel.put("intSampleId",intSampleId);
-		return view;
+			String labelnu = request.getParameter("labelNo");
+			if(labelnu!=null){
+				SatoLabelPrinter satoP = new SatoLabelPrinter();
+				satoP.printSatoLabel(printSampleList);
+				message += " (Labels are available at the Label printer.)";
+			}		
+			
+			ModelAndView mav = new ModelAndView("sampleList");
+			mav.addObject("sampleList", sampleList);
+			mav.addObject("message", message);
+	
+			return mav;
+			
 	}
 
-	protected java.util.Map referenceData(javax.servlet.http.HttpServletRequest request,
-									  java.lang.Object command,Errors errors)
+	protected Map referenceData(HttpServletRequest request,
+									  	Object command,Errors errors)
 							   
 	{
 		Map models = new HashMap();
@@ -156,7 +141,6 @@ public class AddMultiSamplesController extends BasicController {
 		List allProjects = projectManager.getAllProjects();
 		
 		models.put("allSampleTypes",allSampleTypes);
-		
 		
 		log.debug("referenceData is called");
 		models.put("allProjects",allProjects);
