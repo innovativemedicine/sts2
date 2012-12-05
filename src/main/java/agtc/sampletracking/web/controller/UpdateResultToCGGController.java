cc84fcb422ab5da1b8ae76a2d9f20ef322d96dec
imnderoo <nderoo.wong@gmail.com>
2012-12-05 11:21:09 -0500

    Code Cleanup
    Changed Success View.
    Removed unused Imports.

--- src/main/java/agtc/sampletracking/web/controller/UpdateResultToCGGController.java
+++ src/main/java/agtc/sampletracking/web/controller/UpdateResultToCGGController.java
@@ -5,13 +5,10 @@
  * Window - Preferences - Java - Code Style - Code Templates
  */
 package agtc.sampletracking.web.controller;
-import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import org.springframework.web.servlet.*;
-import org.springframework.web.servlet.view.*;
 import agtc.sampletracking.bus.manager.*;
-import agtc.sampletracking.dao.*;
 import org.springframework.validation.BindException;
 /**
  * @author Jianan Xiao, Jan 19, 2005
@@ -29,17 +26,19 @@ public class UpdateResultToCGGController extends BasicController{
 	}
 	
 	protected ModelAndView onSubmit(
-			javax.servlet.http.HttpServletRequest request,
-			javax.servlet.http.HttpServletResponse response, 
-			java.lang.Object command,BindException errors) 
+			HttpServletRequest request,
+			HttpServletResponse response, 
+			Object command,BindException errors) 
 			throws java.lang.Exception{
 		
-		
+		ModelAndView mav = new ModelAndView("updateResultToCGG");
+	
 		String message = agtcManager.updateResultToCGG();
 		message = "Have successfully updated the Genotype Result to CGG <br>" + message;
 
-		//if (al == null) return new ModelAndView(new RedirectView(getSuccessView()));
-		return new ModelAndView("successComplete","message",message);
+		mav.addObject("message", message);
+		
+		return mav;
 	}
 
 	/**
