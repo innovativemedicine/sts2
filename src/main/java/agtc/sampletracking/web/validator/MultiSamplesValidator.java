ce63cb613270336fe5b072dbd5616c2fffa8a08d
imnderoo <nderoo.wong@gmail.com>
2012-12-05 14:28:43 -0500

    Removed debug message and updated pattern

--- src/main/java/agtc/sampletracking/web/validator/MultiSamplesValidator.java
+++ src/main/java/agtc/sampletracking/web/validator/MultiSamplesValidator.java
@@ -30,26 +30,19 @@ public class MultiSamplesValidator implements Validator {
 			for(int i=0; i<multiSamples.getMultiSamples().size(); i++)
 			{
 				System.out.println("Entering Multi Validator");
-				System.out.println("Size:" + multiSamples.getMultiSamples().size());
-				
+//				System.out.println("Size:" + multiSamples.getMultiSamples().size());
+//				
 				Sample s = (Sample)multiSamples.getMultiSamples().get(i);
-				
-				System.out.println("Patient ID:" + s.getPatient().getIntSampleId());
-				
+								
 				String intSampleField = "multiSamples[" + i + "].patient.intSampleId";
 				String extSampleField = "multiSamples[" + i + "].patient.extSampleId";
-				
-				System.out.println(intSampleField);
-				System.out.println(extSampleField);
-				
+			
 				ValidationUtils.rejectIfEmptyOrWhitespace(errors, intSampleField,  "error.required", "Required: Internal ID");
 				ValidationUtils.rejectIfEmptyOrWhitespace(errors, extSampleField,  "error.required", "Required: External ID");
-				
-				Pattern pattern = Pattern.compile("[A-Za-z]{1,4}[0-9]{1,6}");
+
+				Pattern pattern = Pattern.compile("[A-Za-z]{1,6}[0-9]{1,6}");
 				Matcher matcher = pattern.matcher(s.getPatient().getIntSampleId());
-					
-				//log.debug("the matching result is " + matcher.matches());
-				
+									
 					if(!matcher.matches()){
 						errors.rejectValue( intSampleField, "error.wrongFormat","Wrong format: Internal ID");
 			}
