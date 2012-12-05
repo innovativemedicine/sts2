a68514b5a420883424383a244fb2c12461ef9426
imnderoo <nderoo.wong@gmail.com>
2012-12-05 14:33:15 -0500

    Remov searchByProject code. Add getLargestSampleId.

--- src/main/java/agtc/sampletracking/dao/SampleDAO.java
+++ src/main/java/agtc/sampletracking/dao/SampleDAO.java
@@ -18,6 +18,8 @@ import agtc.sampletracking.model.*;
 public interface SampleDAO {
 	public List getSamples();
 	public List getSamples(List sampleIds,List sampleTypeSuffixes,Integer sampleDupNo);
+	public String getLargestSampleId(String intSamplePrefix);
+	// Simple Search searches sampleId using sampleTypeId and projectId;
 	public List simpleSearchSamples(List sampleIds, List sampleTypeIds, List projectIds);
 	public Sample getSample(String sampleId, String sampleTypeSuffix,Integer sampleDupNo);
 	public Sample getSample(Integer sampleId);
@@ -27,7 +29,6 @@ public interface SampleDAO {
 	public Sample getSampleByIntSampleIdUniKey(String intId,SampleType sampleType,Integer sampleDupNo);
 	public List getSampleByIntSampleIdSampleType(String intId,SampleType sampleType);
 	public List getSampleByIntSampleIdSampleType(String intId,String[] sampleType);
-	public List getSampleByIntSampleIdSampleProject(String[] sampleProject);
 	public List getSampleByIntSampleId(String intId);
 	public boolean containsIntSampleId(String intSampleId);
 	public List getExistingSampleTypes(String intSampleId);
