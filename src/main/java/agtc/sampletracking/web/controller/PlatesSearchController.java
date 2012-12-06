069fe3b47b2286aa6c33e769f597d1da43ed2d05
imnderoo <nderoo.wong@gmail.com>
2012-12-06 12:07:37 -0500

    Remove Unused Files

--- src/main/java/agtc/sampletracking/web/controller/PlatesSearchController.java
+++ src/main/java/agtc/sampletracking/web/controller/PlatesSearchController.java
@@ -1,8 +1,11 @@
 /*
+ * Created on Nov 2, 2004
+ *
  * To change the template for this generated file go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
 package agtc.sampletracking.web.controller;
+
 import org.springframework.web.bind.RequestUtils;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,72 +27,90 @@ import agtc.sampletracking.dao.*;
 import agtc.sampletracking.model.*;
 import agtc.sampletracking.web.*;
 import agtc.sampletracking.web.command.*;
-import agtc.sampletracking.web.searchFields.AssaySearchFields;
+import agtc.sampletracking.web.searchFields.ContainerSearchFields;
 import agtc.sampletracking.web.searchFields.OperatorList;
+import agtc.sampletracking.web.searchFields.SampleSearchFields;
+import agtc.sampletracking.bus.SampleListHolder;
 import agtc.sampletracking.bus.manager.*;
-
 import org.springframework.validation.BindException;
 import java.util.*;
 
-
 /**
- * @author Bei Jin
+ * @author Gloria Deng
  *
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class ProjectSamplesSearchController extends BasicSearchController {
-	private ProjectManager projectManager;
-	private Log log = LogFactory.getLog(ProjectsSearchController.class);
+public class PlatesSearchController extends BasicSearchController {
+	private ContainerManager containerManager;
 	
-	/**
-	 * @return
-	 */
-	public ProjectManager getProjectManager() {
-		return projectManager;
-	}
-
-	/**
-	 * @param manager
-	 */
-	public void setProjectManager(ProjectManager manager) {
-		projectManager = manager;
-	}
+	private Log log = LogFactory.getLog(ContainersSearchController.class);
 	
 	protected ModelAndView onSubmit(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response, java.lang.Object command,BindException errors) throws java.lang.Exception{
-		List searchResults = handleSubmit(request,command,"project");
-		
+	
+		List searchResults = handleSubmit(request,command,"container");
+	
 		if (searchResults.size() < 1) {
-			ModelAndView view = new ModelAndView(new RedirectView("projectsamples.htm"));
+			ModelAndView view = new ModelAndView(new RedirectView("containers.htm"));
 			putMessage(request,view.getModel());
 			return view;
 		}
-
-		if (searchResults.size() >1) {
-			// multiple projects found
-			return new ModelAndView("projectList", "projectList", searchResults);
+		
+		String useFor = RequestUtils.getStringParameter(request, "useFor","");
+		
+		if(useFor.equals("useFor")){
+			SampleListHolder plateListHolder = (SampleListHolder)WebUtils.getOrCreateSessionAttribute(request.getSession(),"plateListHolder",SampleListHolder.class);
+			plateListHolder.setOriginalList(searchResults);
+			ModelAndView view = new ModelAndView(new RedirectView("plateList4select.htm"));
+			return view;
 		}
-		
-		Project project = (Project) searchResults.iterator().next();
-		
+
+		if (searchResults.size() > 1) {
+			// multiple projects found
+			return new ModelAndView("containerList", "containerList", searchResults);
+		}
+
+		// only one result
+		Container container = (Container) searchResults.iterator().next();
+
 		ModelAndView view = new ModelAndView(new RedirectView(getSuccessView()));
 		Map myModel = view.getModel();
-		myModel.put("projectId",project.getProjectId());
+		myModel.put("containerId",container.getContainerId());   ;
 		return view;
 	}
 	
 	protected  List performSearch(List crtList,List lgcList){
-		return projectManager.searchProject(crtList,lgcList);
+		return containerManager.searchContainer(crtList,lgcList);
 	}
 
 	protected java.util.Map referenceData(javax.servlet.http.HttpServletRequest request)
 							   throws java.lang.Exception
 	{
 		Map models = new HashMap();
-		referenceData(request,models,"project");
-		List projectList = projectManager.getAllProjects();
-
-		models.put("projectList",projectList);
+		List containerList = containerManager.getAllContainers();
+		
+		referenceData(request,models,"container");
+		models.put("containerList",containerList);
 		return models;
 	}
+
+	
+
+	/**
+	 * @return
+	 */
+	public ContainerManager getContainerManager() {
+		return containerManager;
+	}
+
+	
+
+	/**
+	 * @param manager
+	 */
+	public void setContainerManager(ContainerManager manager) {
+		containerManager = manager;
+	}
+
+	
 }
---
+++ src/main/webapp/WEB-INF/jsp/searchPlates.jsp
@@ -0,0 +1,9 @@
+<%@ include file="/WEB-INF/jsp/includes/head.jsp"%>
+<%@ include file="/WEB-INF/jsp/includes/success.jsp"%>
+
+<h2>Search Plates:</h2>
+
+<%@ include file="/WEB-INF/jsp/includes/searchCore.jsp"%>
+<%@ include file="/WEB-INF/jsp/includes/containerListBody.jsp"%>
+
+<%@ include file="/WEB-INF/jsp/includes/foot.jsp"%>
