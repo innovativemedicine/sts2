630fc1d69bb32540e57c31f35d497af51858e16d
imnderoo <nderoo.wong@gmail.com>
2012-12-12 10:59:58 -0500

    Added child and mother container checks. Added samplebox checks

--- src/main/java/agtc/sampletracking/model/Container.java
+++ src/main/java/agtc/sampletracking/model/Container.java
@@ -14,58 +14,63 @@ import agtc.sampletracking.*;
 */
 public class Container  implements Serializable,ConstantInterface {
 
-    /** identifier field */
     private Integer containerId;
-
-    /** persistent field */
     private String name;
-
-    /** nullable persistent field */
     private String extContainerId;
-
-    /** nullable persistent field */
     private Date createdDate;
-
-    /** nullable persistent field */
     private String comments;
-
-    /** nullable persistent field */
     private String status;
-
-    /** persistent field */
     private Location location;
-
-    /** persistent field */
     private Project project;
-
-    /** persistent field */
     private ContainerType containerType;
-
-    /** persistent field */
     private Set samplesInContainers;
-    
     private Container motherContainer;
-    
     private Set childContainers;
-    
     private Float concentration;
-    
     private boolean noneSample;
-    
     private boolean orderedSample;
-    
     private boolean unorderedSample;
-
     private int totalSamples;
   
     /** default constructor */
     public Container() {
     }
+       
+    public boolean isHasChildContainer(){
+    	if(!childContainers.isEmpty()) {
+    		return true;
+    	}
+    	return false;
+    }
     
-    /** in containerDetails.jsp, call this to check the containerType */
+    public boolean isHasMotherContainer(){
+    	if(motherContainer != null){
+    		return true;
+    	}else{
+    		return false;
+    	}
+    }
+    
     
     public boolean isPlate(){
     	if(containerType !=null && containerType.getName().startsWith(PLATE)){
+    		return true;
+    	}
+    	return false;
+    }
+        
+    public boolean isSampleBox(){
+    	if(containerType !=null && containerType.getName().startsWith(SAMPLE_BOX)){
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public boolean isSampleBoxOrPlate(){
+    	if(containerType !=null && containerType.getName().startsWith(PLATE)){
+    		return true;
+    	}
+    	if(containerType !=null && containerType.getName().startsWith(SAMPLE_BOX)){
     		return true;
     	}
     	return false;
@@ -95,29 +100,6 @@ public class Container  implements Serializable,ConstantInterface {
     	unorderedSample = b;
     }
     
-    /** in containerDetails.jsp, call this to check the containerType */
-    
-    public boolean isSampleBox(){
-    	if(containerType !=null && containerType.getName().startsWith(SAMPLE_BOX)){
-    		return true;
-    	}
-    	return false;
-    }
-    
-    /** in containerDetails.jsp, call this to check the containerType */
-    
-    public boolean isSampleBoxOrPlate(){
-    	if(containerType !=null && containerType.getName().startsWith(PLATE)){
-    		return true;
-    	}
-    	if(containerType !=null && containerType.getName().startsWith(SAMPLE_BOX)){
-    		return true;
-    	}
-    	return false;
-    }
-    
-    /** in containerDetails.jsp, call this to check the containerType */
-    
     public boolean isReagentBox(){
     	if(containerType !=null && containerType.getName().startsWith(REAGENT_BOX)){
     		return true;
@@ -125,34 +107,15 @@ public class Container  implements Serializable,ConstantInterface {
     	return false;
     }
     
-    public boolean isHasMotherContainer(){
-    	if(motherContainer != null){
-    		return true;
-    	}else{
-    		return false;
-    	}
-    }
-    
-   
-	/**
-	 * @return Returns the totalSamples.
-	 */
+
 	public int getTotalSamples() {
 		return samplesInContainers.size();
 	}
-	/**
-	 * @param totalSamples The totalSamples to set.
-	 */
+
 	public void setTotalSamples(int totalSamples) {
 		this.totalSamples = totalSamples;
 	}
-    /** 
-     *            @hibernate.property
-     *             column="NAME"
-     *             length="32"
-     *             not-null="true"
-     *         
-     */
+
     public String getName() {
         return this.name;
     }
@@ -161,12 +124,6 @@ public class Container  implements Serializable,ConstantInterface {
         this.name = name;
     }
 
-    /** 
-     *            @hibernate.property
-     *             column="EXT_CONTAINER_ID"
-     *             length="32"
-     *         
-     */
     public String getExtContainerId() {
         return this.extContainerId;
     }
@@ -175,12 +132,6 @@ public class Container  implements Serializable,ConstantInterface {
         this.extContainerId = extContainerId;
     }
 
-    /** 
-     *            @hibernate.property
-     *             column="CREATED_DATA"
-     *             length="7"
-     *         
-     */
     public Date getCreatedDate() {
         return this.createdDate;
     }
@@ -189,12 +140,6 @@ public class Container  implements Serializable,ConstantInterface {
         this.createdDate = createdDate;
     }
 
-    /** 
-     *            @hibernate.property
-     *             column="COMMENTS"
-     *             length="256"
-     *         
-     */
     public String getComments() {
         return this.comments;
     }
@@ -203,12 +148,6 @@ public class Container  implements Serializable,ConstantInterface {
         this.comments = comments;
     }
 
-    /** 
-     *            @hibernate.property
-     *             column="STATUS"
-     *             length="10"
-     *         
-     */
     public String getStatus() {
         return this.status;
     }
@@ -217,12 +156,6 @@ public class Container  implements Serializable,ConstantInterface {
         this.status = status;
     }
 
-    /** 
-     *            @hibernate.many-to-one
-     *             not-null="true"
-     *            @hibernate.column name="LOCATION_ID"         
-     *         
-     */
     public Location getLocation() {
         return this.location;
     }
@@ -231,12 +164,6 @@ public class Container  implements Serializable,ConstantInterface {
         this.location = location;
     }
 
-    /** 
-     *            @hibernate.many-to-one
-     *             not-null="true"
-     *            @hibernate.column name="PROJECT_ID"         
-     *         
-     */
     public Project getProject() {
         return this.project;
     }
@@ -245,12 +172,6 @@ public class Container  implements Serializable,ConstantInterface {
         this.project = project;
     }
 
-    /** 
-     *            @hibernate.many-to-one
-     *             not-null="true"
-     *            @hibernate.column name="CONTAINER_TYPE_ID"         
-     *         
-     */
     public ContainerType getContainerType() {
         return this.containerType;
     }
@@ -259,70 +180,43 @@ public class Container  implements Serializable,ConstantInterface {
         this.containerType = containerType;
     }
     
-	/**
-	 * @return
-	 */
+
 	public Integer getContainerId() {
 		return containerId;
 	}
 
-	/**
-	 * @param integer
-	 */
 	public void setContainerId(Integer integer) {
 		containerId = integer;
 	}
 
-	/**
-	 * @return
-	 */
 	public Set getSamplesInContainers() {
 		return samplesInContainers;
 	}
 
-	/**
-	 * @param set
-	 */
 	public void setSamplesInContainers(Set set) {
 		samplesInContainers = set;
 	}
 
-
-	/**
-	 * @return Returns the concentration.
-	 */
 	public Float getConcentration() {
 		return concentration;
 	}
-	/**
-	 * @param concentration The concentration to set.
-	 */
+
 	public void setConcentration(Float concentration) {
 		this.concentration = concentration;
 	}
 	
-	
-	/**
-	 * @return Returns the childContainers.
-	 */
 	public Set getChildContainers() {
 		return childContainers;
 	}
-	/**
-	 * @param childContainers The childContainers to set.
-	 */
+
 	public void setChildContainers(Set childContainers) {
 		this.childContainers = childContainers;
 	}
-	/**
-	 * @return Returns the motherContainer.
-	 */
+
 	public Container getMotherContainer() {
 		return motherContainer;
 	}
-	/**
-	 * @param motherContainer The motherContainer to set.
-	 */
+
 	public void setMotherContainer(Container motherContainer) {
 		this.motherContainer = motherContainer;
 	}
