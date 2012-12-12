3f2964b268e27d4d89d05f2348ecc81aef9fe6f0
imnderoo <nderoo.wong@gmail.com>
2012-12-12 11:24:20 -0500

    Updated Barcode Reader to accept Plate barcodes and updated Sample barcodes format
    Need to update Sample barcodes format again to support multiple sample
    type for a single ID

--- src/main/java/agtc/sampletracking/web/controller/BarcodeController.java
+++ src/main/java/agtc/sampletracking/web/controller/BarcodeController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -52,16 +53,15 @@ public class BarcodeController extends BasicController {
 			String barcodeString = barcodeItem.toString();
 			String response = new String() ;
 			
-		if (barcodeString.matches("S-[a-zA-Z]+\\d+-[A-Z]+-\\d+")) {
+		if (barcodeString.matches("S-" + SAMPLE_PREFIX + "\\d+")) {
 			String[] bcArray = barcodeString.split("-");
 			String sampleId = bcArray[1];
-			String sampleTypeSuffix = bcArray[2];
-			Integer sampleDupNo = Integer.parseInt(bcArray[3]);
+
+			Sample sample = sampleManager.getSample(sampleId);
 			
-			Sample sample = sampleManager.getSample(sampleId, sampleTypeSuffix, sampleDupNo);
 			if (sample == null) {
-				response += addSampleFromBarcode(sampleId,  sampleTypeSuffix,  sampleDupNo);
-				
+//				response += addSampleFromBarcode(sampleId,  sampleTypeSuffix,  sampleDupNo);
+				response = "<b>Error</b>(Sample not found in database):";
 			} 
 			else {
 				// Update sample status and sample location
@@ -69,43 +69,119 @@ public class BarcodeController extends BasicController {
 			}
 			
 			response += sampleId;
-			response += sampleTypeSuffix;
-			response += sampleDupNo;
 		}
-			else if(barcodeString.matches("C-\\w+")) {
+		else if(barcodeString.matches("P-" + PLATE_PREFIX + "\\d+")) {
+			String[] bcArray = barcodeString.split("-");
+			String plateId = bcArray[1];
+			
+			Container plate = containerManager.getContainer(plateId);
+			
+			if (plate == null) {
+				response = "<b>Error(Plate not found in database):</b>";
+			}
+			else {
+				response += updatePlateFromBarcode(plate, storeContainer);
+			}
+			
+			response += plateId;
+		}
+		else if(barcodeString.matches("C-\\w+")) {
 				
 				String[] bcArray = barcodeString.split("-");
 				String containerName = bcArray[1];
 				Container container = containerManager.getContainer(containerName);
 				
 				if(container == null) {
-					response += "<b>Error</b>(Contained does not exist in database):";
+					response += "<b>Error(Contained does not exist in database):</b>";
 					//To make this work properly, need to open a add Container form and disable barcodes until form is filled.
 				}
-				else if(containerName.equalsIgnoreCase(storeContainer))
-				{
+				else if(containerName.equalsIgnoreCase(storeContainer)) {
 					response += "<b>FinishedStoreToContainer</b>:";
 				}
-				else
-				{
+				else {
 					response += "<b>StoreToContainer</b>:";
 				}
 				
 				response += containerName;
 			}
-			else
-			{
+			else {
 				response += "<b>Error(Unrecognized barcode format)</b>:";
 				response += barcodeString;
 			}
-
 			return response;
 	 }
 	 
-	 protected String addContainerFromBarcode(String containerName)
-		{
+	 protected String addContainerFromBarcode(String containerName) {
 		 	//containerManager.saveContainer(containerObject);
 		 	return "<b>AddedContainer</b>:";
+		}
+
+	 protected String updatePlateFromBarcode(Container plate, String storeContainer) {
+			String response = "<b>" + plate.getStatus() + "Plate</b>:";
+			String plateStatus = plate.getStatus(); // Default is unchanged
+			Container motherContainer = null;
+			
+			// Do we allow sample to be scanned without an associated container??
+			if (storeContainer.isEmpty()) {
+				if (plateStatus.equalsIgnoreCase("registered")) {
+					response = "<b>ReceivedPlate:</b>";
+					plateStatus = "Received";
+				} else if (plateStatus.equalsIgnoreCase("stored")) {
+					
+					// Retrieving Sample by removing sample from SIC table
+
+					// Remove plate from mother Container
+					motherContainer = plate.getMotherContainer();
+					Set cic = motherContainer.getChildContainers(); //cics = containers in container
+					cic.remove(plate);
+					motherContainer.setChildContainers(cic);
+					
+					// Set plate's mother Container to null
+					plate.setMotherContainer(null);		
+					
+					response = "<b>RetrievedPlate:</b>";
+					plateStatus = "Retrieved";
+				}
+			} else { // Container scanned. Save Samples in Container
+				
+				motherContainer = containerManager.getContainer(storeContainer);
+
+				Set cic = motherContainer.getChildContainers();
+				cic.add(plate);
+				motherContainer.setChildContainers(cic);
+				
+				plate.setMotherContainer(motherContainer);
+				
+				response = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>StoredPlate</b>(";
+				response += storeContainer;
+				response += ")";
+				response += ":";
+				plateStatus = "Stored";
+			}
+
+			// Special case to deal with legacy samples with no status.
+			if (plate.getStatus() == null) {
+				plateStatus = "Registered";
+				response = "<b>RegisteredPlate:</b>";
+			}
+			
+			// Update Plate Status
+			try {
+				plate.setStatus(plateStatus);
+				containerManager.saveContainer(plate);
+				if (motherContainer != null)
+				{
+					containerManager.saveContainer(motherContainer);
+				}
+
+			} catch (Exception e) {
+				String errorMsg = e.getMessage();
+				e.printStackTrace();
+				response = "<b>Error(" + errorMsg + "):</b>";
+				return response;
+			}
+			
+			return response;
 		}
 
 	protected String updateSampleFromBarcode(Sample sample, String storeContainer) {
@@ -150,8 +226,9 @@ public class BarcodeController extends BasicController {
 				sampleStatus = "Stored";
 
 			} catch (Exception e) {
-				response = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Error</b>(Can't store sample)";
+				response = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Error(Can't store sample)</b>";
 				e.printStackTrace();
+				return response;
 			}
 		}
 
@@ -165,47 +242,47 @@ public class BarcodeController extends BasicController {
 		try {
 			sampleManager.updateSampleStatus(sample, sampleStatus);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			response = "<b>Error</b>(Failed to update sample):";
+			response = "<b>Error(Failed to update sample):</b>";
+			e.printStackTrace();
+			return response;
 		}
 		
 		return response;
 	}
 
-	 protected String addSampleFromBarcode(String sampleId, String sampleTypeSuffix, Integer sampleDupNo)
-		{
-		 	String response = "";
-		 	
-			Sample newSample = new Sample();
-				
-			SampleType st = agtcManager.getSampleTypeBySuffix(sampleTypeSuffix);
-
-			if (st == null) {
-				response = "<b>Error</b>(Unrecognized sample type):";
-			} 
-			else {
-				newSample.setSampleType(st);
-				newSample.setSampleDupNo(sampleDupNo);
-				
-				Patient patient = new Patient(sampleId);
-				newSample.setPatient(patient);
-				
-				try {
-					sampleManager.saveSample(newSample);
-					response = "<b>AddedSample</b>:";
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					response = "<b>Error</b>(Failed to save sample):";
-					e.printStackTrace();
-				}
-				// Activate if we want to prompt user to fill in patient info
-//				patient = sampleManager.getPatient(sampleId);
-//				if(patient != null){
-//					response += "AddPatientAndSample:";
+//	 protected String addSampleFromBarcode(String sampleId, String sampleTypeSuffix, Integer sampleDupNo)
+//		{
+//		 	String response = "";
+//		 	
+//			Sample newSample = new Sample();
+//				
+//			SampleType st = agtcManager.getSampleTypeBySuffix(sampleTypeSuffix);
+//
+//			if (st == null) {
+//				response = "<b>Error</b>(Unrecognized sample type):";
+//			} 
+//			else {
+//				newSample.setSampleType(st);
+//				newSample.setSampleDupNo(sampleDupNo);
+//				
+//				Patient patient = new Patient(sampleId);
+//				newSample.setPatient(patient);
+//				
+//				try {
+//					sampleManager.saveSample(newSample);
+//					response = "<b>AddedSample</b>:";
+//				} catch (Exception e) {
+//					response = "<b>Error</b>(Failed to save sample):";
+//					e.printStackTrace();
 //				}
-			}
-				return response;
-		}
+//				// Activate if we want to prompt user to fill in patient info
+////				patient = sampleManager.getPatient(sampleId);
+////				if(patient != null){
+////					response += "AddPatientAndSample:";
+////				}
+//			}
+//				return response;
+//		}
 	 
 	protected void handleSubmit(HttpServletRequest request, Object command, Map model) {
 		BarcodeCommand barcodeItem = (BarcodeCommand) command;
