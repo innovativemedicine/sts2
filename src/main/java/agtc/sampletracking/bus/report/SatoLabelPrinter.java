bbde7e20003b059cce504f2ec2286a2b1e6ff7c6
imnderoo <nderoo.wong@gmail.com>
2013-07-04 11:01:41 -0400

    Updated sample and plate label printer to print CMD file instead of DAT file
    Requires additional header to indicate label, printer, and field in
    compliance to the format described by Labview 7.

--- src/main/java/agtc/sampletracking/bus/report/SatoLabelPrinter.java
+++ src/main/java/agtc/sampletracking/bus/report/SatoLabelPrinter.java
@@ -9,33 +9,58 @@ package agtc.sampletracking.bus.report;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.text.*;
 
 import agtc.sampletracking.model.Container;
 import agtc.sampletracking.model.Sample;
-import agtc.sampletracking.bus.comparator.*;
+import agtc.sampletracking.ConstantInterface;
 
-/**
- * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
- */
-public class SatoLabelPrinter {
-	private SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy ");
+public class SatoLabelPrinter implements ConstantInterface {
 
-	public void printSampleLabel(List sampleList) throws Exception {
+	public void printSampleLabel(List sampleList, String contextPath)
+			throws Exception {
 
 		Iterator ir = sampleList.iterator();
 		// String filename="e:\\commandFiles\\"+UnqueString.UnqueStr()+".DAT";
-		SimpleDateFormat timeStamp = new SimpleDateFormat("ddMMMyyyykkmm");
+		SimpleDateFormat timeStamp = new SimpleDateFormat("ddMMMyyyykkmmss");
 		String fileTime = timeStamp.format(new Date());
-		String filename = "/Users/nderoo324/Documents/Workspace/sts2/label/samples_"
-				+ fileTime + ".dat";
+		String filename = contextPath + LABELPATH + "samples_" + fileTime
+				+ ".cmd";
 		File outputFile = new File(filename);
 		FileWriter out = new FileWriter(outputFile);
 
 		StringBuffer content = new StringBuffer();
 
+		content.append("LABELNAME=\"C:\\LVWIN70\\SAMPLES\\SAMPLE2.LBL\"\r\n");
+		content.append("PRINTER=\"SATO CL-412e - (V) on COM1:\"\r\n");
+		content.append("DATATYPE=DELIMITED\r\n");
+		content.append("DELIMITER=,\r\n");
+		content.append("FIELD=EXTID11\r\n");
+		content.append("FIELD=EXTID12\r\n");
+		content.append("FIELD=INTID1\r\n");
+		content.append("FIELD=SUFFIX1\r\n");
+		content.append("FIELD=BARCODE1\r\n");
+		content.append("FIELD=TOPLINE12\r\n");
+		content.append("FIELD=TOPLINE22\r\n");
+		content.append("FIELD=SUFFIX2\r\n");
+		content.append("FIELD=TOPLINECD12\r\n");
+		content.append("FIELD=TOPLINECD22\r\n");
+		content.append("FIELD=SUFFIX4\r\n");
+		content.append("FIELD=EXTID21\r\n");
+		content.append("FIELD=EXTID22\r\n");
+		content.append("FIELD=INTID\r\n");
+		content.append("FIELD=SUFFIX3\r\n");
+		content.append("FIELD=BARCODE2\r\n");
+		content.append("FIELD=DATE1\r\n");
+		content.append("FIELD=DATE2\r\n");
+		content.append("FIELD=CONCENTRATION1\r\n");
+		content.append("FIELD=CONCENTRATION2\r\n");
+		content.append("FIELD=dupNo1\r\n");
+		content.append("FIELD=dupNo2\r\n");
+		content.append("LABELDATA=THISFILE\r\n");
+		
 		while (ir.hasNext()) {
 
 			Sample sample = (Sample) ir.next();
@@ -43,117 +68,129 @@ public class SatoLabelPrinter {
 			String externalId = sample.getPatient().getExtSampleId();
 			String sampleTypeSuffix = sample.getSampleType().getSuffix();
 			String sampleDupNo = sample.getSampleDupNo().toString();
+			SimpleDateFormat receivedDateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+			//String receivedDate = receivedDateFormat.format(sample.getReceiveDate());
+			String receivedDate = receivedDateFormat.format(new Date());
 
+			String intIdPre = "";
+			String intIdNum = "";
+			
+			Pattern pattern = Pattern.compile("([a-zA-Z]+?)(\\d+)");
+			
+			Matcher m = pattern.matcher(internalId);
+			while(m.find()){
+				intIdPre = m.group(1);
+				intIdNum = m.group(2);
+			}
+			
+			
 			if (externalId == null) {
 				externalId = "";
 			}
-			// For the barcode: It should be in the format
-			// S-internalId-sampleTypeSuffix
-			content.append("S").append("-");
-			content.append(internalId).append("-");
-			content.append(sampleTypeSuffix).append(",");
-			// Also include the external ID and internal ID as text string if
-			// there's room
-			content.append(externalId).append(",");
-			content.append(internalId).append(",");
 
-			content.append("\n");
+			//content.append("FIELD=EXTID11\n");
+			content.append(externalId).append(",");
+//			content.append("FIELD=EXTID12\n");
+			content.append("").append(",");
+//			content.append("FIELD=INTID1\n");
+			content.append(internalId).append(",");
+//			content.append("FIELD=SUFFIX1\n");
+			content.append(sampleTypeSuffix).append(",");
+//			content.append("FIELD=BARCODE1\n");
+			content.append("S-").append(internalId).append(",");
+//			content.append("FIELD=TOPLINE12\n");
+			content.append(intIdPre).append(",");
+//			content.append("FIELD=TOPLINE22\n");
+			content.append(intIdNum).append(",");
+//			content.append("FIELD=SUFFIX2\n");
+			content.append(sampleTypeSuffix).append(",");
+//			content.append("FIELD=TOPLINECD12\n");
+			content.append(intIdPre).append(",");
+//			content.append("FIELD=TOPLINECD22\n");
+			content.append(intIdNum).append(",");
+//			content.append("FIELD=SUFFIX4\n");
+			content.append(sampleTypeSuffix).append(",");
+//			content.append("FIELD=EXTID21\n");
+			content.append(externalId).append(",");
+//			content.append("FIELD=EXTID22\n");
+			content.append("").append(",");
+//			content.append("FIELD=INTID\n");
+			content.append(internalId).append(",");
+//			content.append("FIELD=SUFFIX3\n");
+			content.append(sampleTypeSuffix).append(",");
+//			content.append("FIELD=BARCODE2\n");
+			content.append("S-").append(internalId).append(",");
+//			content.append("FIELD=DATE1\n");
+			content.append(receivedDate).append(",");
+//			content.append("FIELD=DATE2\n");
+			content.append(receivedDate).append(",");
+//			content.append("FIELD=CONCENTRATION1\n");
+			content.append("").append(",");
+//			content.append("FIELD=CONCENTRATION2\n");
+			content.append("").append(",");
+//			content.append("FIELD=dupNo1\n");
+			content.append(sampleDupNo).append(",");
+//			content.append("FIELD=dupNo2\n");
+			content.append(sampleDupNo).append("");
+
+			content.append("\r\n");
 		}
 		out.write(content.toString());
 		out.close();
 	}
 
-	public void printPlateLabel(List plateList) throws Exception {
-		Iterator ir = plateList.iterator();
-		
+	/*
+	 * Probably have to update this to printContainerLabel. Then during the
+	 * ir.hasNext() loop, check if the container is a plate or a box. If it is a
+	 * box, then print out labels for actual containers (C-prefix) If it is a
+	 * plate, then print out labels for plates (P-prefix)
+	 */
+
+	public void printPlateLabel(List<Container> plateList, String contextPath)
+			throws Exception {
+		Iterator<Container> ir = plateList.iterator();
+
 		// String filename="e:\\commandFiles\\"+UnqueString.UnqueStr()+".DAT";
-		SimpleDateFormat timeStamp = new SimpleDateFormat("ddMMMyyyykkmm");
+		SimpleDateFormat timeStamp = new SimpleDateFormat("ddMMMyyyykkmmss");
 		String fileTime = timeStamp.format(new Date());
-		String filename = "/Users/nderoo324/Documents/Workspace/sts2/label/plates_"
-				+ fileTime + ".dat";
+		String filename = contextPath + LABELPATH + "plates_" + fileTime
+				+ ".cmd";
 		File outputFile = new File(filename);
 		FileWriter out = new FileWriter(outputFile);
 
 		StringBuffer content = new StringBuffer();
-		
+
+		content.append("LABELNAME=\"C:\\LVWIN70\\SAMPLES\\PLATETEST2.LBL\"\r\n");
+		content.append("PRINTER=\"ZDesigner ZM400 200 dpi (ZPL) on Ne00:\"\r\n");
+		content.append("DATATYPE=DELIMITED\r\n");
+		content.append("DELIMITER=,\r\n");
+		content.append("FIELD=2DBAR1\r\n");
+		content.append("FIELD=INTID1\r\n");
+		content.append("FIELD=EXTID1\r\n");
+		content.append("LABELDATA=THISFILE\r\n");
+
 		while (ir.hasNext()) {
 			Container plate = (Container) ir.next();
-			printPlateLabel(plate);
-			
+			// printPlateLabel(plate, contextPath);
+
 			String internalId = plate.getName();
 			String externalId = plate.getExtContainerId();
 
 			if (externalId.isEmpty()) {
 				externalId = "";
 			}
+
+			// FIELD=2DBAR1
 			// For the barcode: It should be in the format P-internalId
-			content.append("P").append("-");
+			content.append("P-").append(internalId).append(",");
+			// FIELD=INTID1
 			content.append(internalId).append(",");
-			// Also include the external ID and internal ID as text string if
-			// there's room
-			content.append(externalId).append(",");
-			content.append(internalId).append(",");
-			content.append("\n");
-
+			// FIELD=EXTID1
+			content.append(externalId).append("\r\n");
 		}
-		
-		out.write(content.toString());
-		out.close();
-		
-	}
-	public void printPlateLabel(Container plate) throws Exception {
-
-		// String filename="e:\\commandFiles\\"+UnqueString.UnqueStr()+".DAT";
-		SimpleDateFormat timeStamp = new SimpleDateFormat("ddMMMyyyykkmm");
-		String fileTime = timeStamp.format(new Date());
-		String filename = "/Users/nderoo324/Documents/Workspace/sts2/label/plates_"
-				+ fileTime + ".dat";
-		File outputFile = new File(filename);
-		FileWriter out = new FileWriter(outputFile);
-
-		StringBuffer content = new StringBuffer();
-
-		String internalId = plate.getName();
-		String externalId = plate.getExtContainerId();
-
-		if (externalId.isEmpty()) {
-			externalId = "";
-		}
-		// For the barcode: It should be in the format P-internalId
-		content.append("P").append("-");
-		content.append(internalId).append(",");
-		// Also include the external ID and internal ID as text string if
-		// there's room
-		content.append(externalId).append(",");
-		content.append(internalId).append(",");
-		content.append("\n");
 
 		out.write(content.toString());
 		out.close();
+
 	}
-
-	public void printBloodLabel(List sampleList) throws Exception {
-		Iterator ir = sampleList.iterator();
-		// String filename="e:\\commandFiles\\"+UnqueString.UnqueStr()+".DAT";
-		SimpleDateFormat timeStamp = new SimpleDateFormat("ddMMMyyyykkmm");
-		String fileTime = timeStamp.format(new Date());
-		String filename = "/Users/nderoo324/Documents/Workspace/sts2/label/bloods"
-				+ fileTime + ".dat";
-		File outputFile = new File(filename);
-		FileWriter out = new FileWriter(outputFile);
-
-		StringBuffer content = new StringBuffer();
-
-		while (ir.hasNext()) {
-
-			String sampleId = (String) ir.next();
-
-			content.append(sampleId).append(",");
-			content.append(sampleId).append(",");
-			content.append("\n");
-		}
-		out.write(content.toString());
-		out.close();
-	}
-
 }
