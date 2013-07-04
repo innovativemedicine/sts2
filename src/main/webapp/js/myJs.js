f3f98ac1d354cd6e4abee818a514142bca1a1ec8
imnderoo <nderoo.wong@gmail.com>
2013-07-04 13:22:32 -0400

    Minor formatting change.

--- src/main/webapp/js/myJs.js
+++ src/main/webapp/js/myJs.js
@@ -8,8 +8,8 @@ var oldVal;
 $('.barcodeInput').bind('input', function(event) {
 	var val = this.value;
 	if (val != oldVal) {
-	    oldVal = val;
-	    $('.barcodeAdd').trigger('click');
+		oldVal = val;
+		$('.barcodeAdd').trigger('click');
 	}
 });
 
@@ -20,36 +20,37 @@ $('.selectNav').change(function() {
 $('.generateForm').bind('input', function(event) {
 	var ns = $('.generateForm').val();
 	var sp = $('#sampleIdPreForm').val().toUpperCase();
-	
-	if(ns.match(/^\d+$/)) {
+
+	if (ns.match(/^\d+$/)) {
 		if (ns >= 3) {
 			ns = 3;
 		}
-	}
-	else {
+	} else {
 		ns = 0;
 	}
-	
-	if(!$.trim(sp).length) {
-		location.search = $.param({'ns':ns});
-	}
-	else {
-	//	reload page with attached param
-		location.search = $.param({'ns':ns, 'sp':sp});
+
+	if (!$.trim(sp).length) {
+		location.search = $.param({
+			'ns' : ns
+		});
+	} else {
+		// reload page with attached param
+		location.search = $.param({
+			'ns' : ns,
+			'sp' : sp
+		});
 	}
 });
 
 $('.unhider').click(function() {
 	if ($('.hide').is(':visible')) {
 		$('.hide').fadeToggle();
-		if($('.unhider').text() == '[Hide]' )
-		{
+		if ($('.unhider').text() == '[Hide]') {
 			$('.unhider').text('[Show]');
 		}
 	} else {
 		$('.hide').fadeToggle();
-		if($('.unhider').text() == '[Show]' )
-		{
+		if ($('.unhider').text() == '[Show]') {
 			$('.unhider').text('[Hide]');
 		}
 	}
@@ -59,7 +60,6 @@ $('.unhider2').click(function() {
 	if (!($('.hide2').is(':visible'))) {
 		$('.hide1').hide();
 		$('.hide2').fadeIn();
-	
 
 	}
 });
