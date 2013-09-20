9f9097f4dbf75ce34b12484a0cfe2128324bcc34
imnderoo <nderoo.wong@gmail.com>
2013-09-20 17:13:01 -0400

    Rewrote the stSelect portion for Registering Sample and handling pesudo checkboxes

--- src/main/webapp/js/myJs.js
+++ src/main/webapp/js/myJs.js
@@ -15,9 +15,8 @@ $('.barcodeInput').bind('input', function(event) {
 
 // Universal setting for datepicker object. 
 // The Date format of datepicker is specified in the textbox using data-date-format="dd-mm-yyyy" 
-$('.datepicker').datepicker(
-{
-	autoclose: true
+$('.datepicker').datepicker({
+	autoclose : true
 });
 
 // Linked button is used in registerMultiple Samples to link the button to a submit button
@@ -33,28 +32,56 @@ $('.selectNav').change(function() {
 
 // Helper function to return hex instead of RGB
 $.fn.getHexBgColour = function() {
-    var rgb = $(this).css('background-color');
-    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    function hex(x) {return ("0" + parseInt(x).toString(16)).slice(-2);}
-    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+	var rgb = $(this).css('background-color');
+	rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+	function hex(x) {
+		return ("0" + parseInt(x).toString(16)).slice(-2);
+	}
+	return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 };
 
-//clickSelect is used for Sample Type Number and Selection in Register Sample
-$('.clickSelect').click(function() {
-	this.value = this.placeholder;
-	//alert(this.placeholder);
-	this.select();
-});
+//stSelect and stValue are used for Sample Type Number and Selection in Register Sample
+//$('.clickSelect').click(function() {
+//	this.value = this.placeholder;
+//	//alert(this.placeholder);
+//	this.select();
+//});
 
-$('.clickSelect').blur(function() {
-	
-	var selectValue = this.value;
-	var labelId = '#' + this.name + '_label';
-		
-	if(selectValue > 0) {
-		$(labelId).css('background-color', '#ffffff');
+//$('.clickSelect').click(function() {
+//	
+//	var selectValue = this.value;
+//	var labelId = '#' + this.name + '_label';
+//		
+//	if(selectValue > 0) {
+//		$(labelId).css('background-color', '#d9edf7');
+//	}
+//	else {
+//		$(labelId).css('background-color', '#eeeeee');
+//	}
+//});
+
+$('.stSelect').click(function() {
+	//	
+	//	var selectValue = this.value;
+	//	var labelId = '#' + this.name + '_label';
+	//		
+	//	alert($(this).getHexBgColour());	
+
+	var stValId = '#' + this.id + '_value';
+
+	// If activated, deactivate
+	if ($(this).hasClass('active')) {
+		$(this).css('background-color', '#e6e6e6');
+		$(stValId).attr('disabled', 'disabled');
+		$(stValId).val('');
 	}
+	// If decativated, activate
 	else {
-		$(labelId).css('background-color', '#eeeeee');
+		$(this).css('background-color', '#d9edf7');
+
+		$(stValId).removeAttr('disabled');
+		$(stValId).val($(stValId).attr('placeholder'));
+		$(stValId).select();
 	}
+
 });
