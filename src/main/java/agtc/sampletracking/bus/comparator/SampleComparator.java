6d9acc85550625e6cac1ab5172828533ee4763aa
imnderoo <nderoo.wong@gmail.com>
2013-09-25 18:09:11 -0400

    Updated SampleComaprator to order by intSampleId, Suffix, and DupNo

--- src/main/java/agtc/sampletracking/bus/comparator/SampleComparator.java
+++ src/main/java/agtc/sampletracking/bus/comparator/SampleComparator.java
@@ -11,12 +11,14 @@ import agtc.sampletracking.model.*;
 
 /**
  * @author Gloria
- *
- * compare two samples, to sort sample list
+ * 
+ *         compare two samples, to sort sample list
  */
 public class SampleComparator implements Comparator {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
 	 */
 	public int compare(Object o1, Object o2) {
@@ -24,7 +26,26 @@ public class SampleComparator implements Comparator {
 		Sample s2 = (Sample)o2;
 		String interlId1 = s1.getPatient().getIntSampleId();
 		String interlId2 = s2.getPatient().getIntSampleId();
-		return interlId1.compareTo(interlId2);
+		int compareResult = interlId1.compareTo(interlId2);
+		
+		// If Int sample ID are equal, then sort by sample type suffix
+		if (compareResult == 0)
+		{
+			String st1 = s1.getSampleType().getSuffix();
+			String st2 = s2.getSampleType().getSuffix();
+			
+			compareResult = st1.compareTo(st2);
+		}
+		
+		// If sample type suffix are equal, then sort by dup number
+		if (compareResult == 0)
+		{
+			Integer num1 = s1.getSampleDupNo();
+			Integer num2 = s2.getSampleDupNo();
+			
+			compareResult = num1.compareTo(num2);
+		}
+		
+		return compareResult;
 	}
-
 }
