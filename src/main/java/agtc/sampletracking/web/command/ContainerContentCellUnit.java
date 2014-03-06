9d9ed5af06f214fc94900c3ae2779c9700548331
imnderoo <nderoo.wong@gmail.com>
2014-03-06 11:25:15 -0500

    Updated display for sampleDesc in container content

--- src/main/java/agtc/sampletracking/web/command/ContainerContentCellUnit.java
+++ src/main/java/agtc/sampletracking/web/command/ContainerContentCellUnit.java
@@ -5,101 +5,112 @@
  * Window - Preferences - Java - Code Style - Code Templates
  */
 package agtc.sampletracking.web.command;
+
 import agtc.sampletracking.model.*;
+
 /**
- * @author Hongjing
- * Window - Preferences - Java - Code Style - Code Templates
+ * @author Hongjing Window - Preferences - Java - Code Style - Code Templates
  */
 public class ContainerContentCellUnit {
-	private String sampleDesc;
-	private Integer sicId;
-	private String sampleTypeSuffix;
-	private String well;
-	private String dupNo;
-	private boolean notOccupied;
-	
-	public ContainerContentCellUnit(SamplesInContainer samplesInContainer){
-		if(samplesInContainer!=null){
+	private String	sampleDesc;
+	private Integer	sicId;
+	private String	sampleTypeSuffix;
+	private String	well;
+	private String	dupNo;
+	private boolean	notOccupied;
+
+	public ContainerContentCellUnit(SamplesInContainer samplesInContainer) {
+		if (samplesInContainer != null) {
 			Sample sample = samplesInContainer.getSample();
-			if(sample!=null){
-				sampleDesc = sample.getPatient().getIntSampleId() + " "+ sample.getSampleType().getSuffix();
-				if(sample.getSampleDupNo()!=null && sample.getSampleDupNo().intValue()!=1){
-					sampleDesc += "("+sample.getSampleDupNo().toString()+")";
-				}
+			if (sample != null) {
+				sampleDesc = sample.getPatient().getIntSampleId() + "-" + sample.getSampleType().getSuffix() + "-"
+						+ sample.getSampleDupNo();
+
 				sicId = samplesInContainer.getSicId();
 				well = samplesInContainer.getWell();
 				notOccupied = false;
-			}else{
+			} else {
 				notOccupied = true;
 			}
-		}else{
+		} else {
 			notOccupied = true;
 		}
 	}
-	public boolean isNotOccupied(){
+
+	public boolean isNotOccupied() {
 		return notOccupied;
 	}
-	
+
 	/**
 	 * @return Returns the sampleDesc.
 	 */
 	public String getSampleDesc() {
 		return sampleDesc;
 	}
+
 	/**
-	 * @param sampleDesc The sampleDesc to set.
+	 * @param sampleDesc
+	 *            The sampleDesc to set.
 	 */
 	public void setSampleDesc(String sampleDesc) {
 		this.sampleDesc = sampleDesc;
 	}
-	
+
 	/**
 	 * @return Returns the sicId.
 	 */
 	public Integer getSicId() {
 		return sicId;
 	}
+
 	/**
-	 * @param sicId The sicId to set.
+	 * @param sicId
+	 *            The sicId to set.
 	 */
 	public void setSicId(Integer sicId) {
 		this.sicId = sicId;
 	}
+
 	/**
 	 * @return Returns the sampleTypeSuffix.
 	 */
 	public String getSampleTypeSuffix() {
 		return sampleTypeSuffix;
 	}
+
 	/**
-	 * @param sampleTypeSuffix The sampleTypeSuffix to set.
+	 * @param sampleTypeSuffix
+	 *            The sampleTypeSuffix to set.
 	 */
 	public void setSampleTypeSuffix(String sampleTypeSuffix) {
 		this.sampleTypeSuffix = sampleTypeSuffix;
 	}
-	
+
 	/**
 	 * @return Returns the well.
 	 */
 	public String getWell() {
 		return well;
 	}
+
 	/**
-	 * @param well The well to set.
+	 * @param well
+	 *            The well to set.
 	 */
 	public void setWell(String well) {
 		this.well = well;
 	}
-	
-	
+
 	/**
 	 * @return Returns the dupNo.
 	 */
 	public String getDupNo() {
 		return dupNo;
 	}
+
 	/**
-	 * @param dupNo The dupNo to set.
+	 * @param dupNo
+	 *            The dupNo to set.
 	 */
 	public void setDupNo(String dupNo) {
 		this.dupNo = dupNo;
